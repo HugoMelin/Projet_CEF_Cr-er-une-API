130c353404251988ecc8e33186134dc9a32978aa
HugoMelin <hugo.melin01@gmail.com>
2024-08-13 22:59:42 +0200

    Add authenticate middleware

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 const service = require('../services/users');
 
+const private = require('../middlewares/private')
+
 router.get('/:id', service.getById);
 
 router.post('/', service.add);
 
 router.patch('/:id', service.update);
 
-router.delete('/:id', service.delete);
+router.delete('/:id', private.checkJWT, service.delete);
 
 router.post('/authenticate', service.authenticate);
 
